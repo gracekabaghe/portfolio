6de40103bdc1beca18fd1abb6b62061880943c82
gracekabaghe <gracema2002@gmail.com>
2021-10-14 18:42:05 +0200

    Added email validation

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ menuItems.forEach(
     menuItem.addEventListener('click', toggleMenu);
   },
 );
+
+// form validation
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const textArea = document.getElementById('message');
+const errorElement = document.getElementById('error');
+function validateEmail(email) {
+  const regEx = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  if (regEx.test(email)) {
+    return true;
+  }
+  return false;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!validateEmail(email.value)) {
+    errorElement.innerText = `The email address needs to be in lowercase EG: ${email.value.toLocaleLowerCase()}`;
+  } else {
+    form.submit();
+  }
+});
+
